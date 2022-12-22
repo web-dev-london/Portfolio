95702f840ed2d1add823c5225ecdb8264db55ace
Web__Dev <murod.london@gmail.com>
2022-12-22 20:38:36 +0000

    Builded for Production

---
+++ dist/index.d709b26f.js
@@ -0,0 +1,2 @@
+const collapsibles=document.querySelectorAll(".collapsible");collapsibles.forEach((l=>l.addEventListener("click",(function(){this.classList.toggle("collapsible--expanded")}))));
+//# sourceMappingURL=index.d709b26f.js.map
