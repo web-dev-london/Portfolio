d52b7963fbf40a9b467f310cd594d31d91570254
Web__Dev <murod.london@gmail.com>
2022-12-22 21:41:06 +0000

    removed animation on scroll

---
+++ dist/index.1240f1f7.js
@@ -0,0 +1,2 @@
+document.querySelectorAll(".collapsible").forEach((e=>e.addEventListener("click",(function(){this.classList.toggle("collapsible--expanded")}))));
+//# sourceMappingURL=index.1240f1f7.js.map
---
+++ dist/index.6cfc6f12.js
@@ -0,0 +1,2 @@
+document.querySelectorAll(".collapsible").forEach((e=>e.addEventListener("click",(function(){this.classList.toggle("collapsible--expanded")}))));
+//# sourceMappingURL=index.6cfc6f12.js.map
